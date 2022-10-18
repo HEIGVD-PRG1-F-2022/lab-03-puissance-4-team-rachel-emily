2cc8fcea37db346b90866743d2343105aa36bc82
rachel <rachel.tranchida@gmail.com>
2022-10-18 15:49:03 +0200

    added affichage function

---
+++ affichage.h
@@ -0,0 +1,10 @@
+//
+// Created by rache on 18.10.2022.
+//
+
+#ifndef MANDELBROT_AFFICHAGE_H
+#define MANDELBROT_AFFICHAGE_H
+#include <vector>
+#include <iomanip>
+void afficheGrille(const std::vector<std::vector<int>> grille);
+#endif //MANDELBROT_AFFICHAGE_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
+#include <vector>
+#include "affichage.h"
 using namespace std;
 
+vector<vector<int>> grille(6, vector<int>(7,0));
 int main()  {
-
+    afficheGrille(grille);
     return 0;
 }
